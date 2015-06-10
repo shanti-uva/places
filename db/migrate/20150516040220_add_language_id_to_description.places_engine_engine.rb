31ebc06c2b557cb922e75e29f127c9be5618fade
Andres Montano <amontano@virginia.edu>
2015-06-09 19:41:21 -0600

    updating engines

---
+++ db/migrate/20150516040220_add_language_id_to_description.places_engine_engine.rb
@@ -0,0 +1,14 @@
+# This migration comes from places_engine_engine (originally 20150508165151)
+class AddLanguageIdToDescription < ActiveRecord::Migration
+  def up
+    add_column :descriptions, :language_id, :integer
+    Description.reset_column_information
+    eng = Language.get_by_code('eng').id
+    Description.where(language_id: nil).update_all(language_id: eng)
+    change_column :descriptions, :language_id, :integer, :null => false
+  end
+  
+  def down
+    remove_column :descriptions, :language_id
+  end
+end
