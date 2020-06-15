92edf58d5880c8c79dc03867fdf7c40f11ba7770
Derik Ramirez <rderik@gmail.com>
2020-06-15 12:54:19 -0600

    Updated engines and imported kmaps migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_155920) do
+ActiveRecord::Schema.define(version: 2020_06_15_185222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -358,6 +358,15 @@ ActiveRecord::Schema.define(version: 2020_06_05_155920) do
     t.string "name", limit: 27
     t.float "elect_use"
     t.geometry "the_geom", limit: {:srid=>4326, :type=>"st_polygon"}
+  end
+
+  create_table "essays", force: :cascade do |t|
+    t.bigint "feature_id", null: false
+    t.integer "text_id", null: false
+    t.integer "language_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_id"], name: "index_essays_on_feature_id"
   end
 
   create_table "external_pictures", id: :serial, force: :cascade do |t|
@@ -1074,4 +1083,5 @@ ActiveRecord::Schema.define(version: 2020_06_05_155920) do
     t.index ["the_geom"], name: "zhol_the_geom_gist", using: :gist
   end
 
+  add_foreign_key "essays", "features"
 end
